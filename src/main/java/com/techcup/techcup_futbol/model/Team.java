3ebe4b49223f0b3aa44ca37c66a178506f3f6add
Shadday123 <shadday539@gmail.com>
2026-03-10 08:21:04 -0500

    Actualización Torneo

--- src/main/java/com/techcup/techcup_futbol/model/Team.java
+++ src/main/java/com/techcup/techcup_futbol/model/Team.java
@@ -1,5 +1,9 @@
 package com.techcup.techcup_futbol.model;
 
+
+import lombok.Data;
+
+@Data
 public class Team {
     public Team(int i, String barcelona) {
     }
--- src/main/java/com/techcup/techcup_futbol/model/Torneo.java
+++ src/main/java/com/techcup/techcup_futbol/model/Torneo.java
@@ -1,7 +1,11 @@
 package com.techcup.techcup_futbol.model;
 
+import lombok.Data;
+
 import java.util.Date;
 
+
+@Data
 public class Torneo {
     private Date fechainicial;
     private Date fechaFinal;
