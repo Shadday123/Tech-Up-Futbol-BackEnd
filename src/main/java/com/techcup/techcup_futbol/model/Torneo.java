1c831554fb4bfe998f28699d5551f9f60da44178
Shadday123 <shadday539@gmail.com>
2026-03-10 07:56:51 -0500

    Actualización Torneo

---
+++ src/main/java/com/techcup/techcup_futbol/model/Torneo.java
@@ -0,0 +1,12 @@
+package com.techcup.techcup_futbol.model;
+
+import java.util.Date;
+
+public class Torneo {
+    private Date fechainicial;
+    private Date fechaFinal;
+    private int cantEquipos;
+    private double costoPorEquipo;
+    private EstadoTorneo estado;
+
+}
